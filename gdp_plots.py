f7be5c520a6a246aa33825b24ec8c16a2c9c90f9
Chelsea Crooks <cmc0043@gmail.com>
2018-08-30 15:02:44 -0500

    Allows user to set '-a' to all gdp csv files

--- gdp_plots.py
+++ gdp_plots.py
@@ -4,12 +4,15 @@ import pandas
 # because we are no longer in a notebook
 import matplotlib.pyplot as plt
 import sys
+import glob
 
 # load data and transpose so that country names are
 # the columns and their gdp data becomes the rows
 
-#define filename
-file_list=sys.argv[1:]
+if sys.argv[1] == '-a' :
+	file_list=glob.glob("*gdp*csv")
+else: 
+	file_list=sys.argv[1:]
 
 for filename in file_list:
 
